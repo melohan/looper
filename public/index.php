02305ca5bfc44723f2635450d2926a4bb32da752
Sou <souphakone.samoutphonh@cpnv.ch>
2021-11-23 01:05:28 +0100

    added delete function

--- public/index.php
+++ public/index.php
@@ -28,6 +28,7 @@ $router->get('/exercise/manage', 'App\Controllers\exerciseController@manage');
 $router->post('/exercise/create', 'App\Controllers\exerciseController@create');
 $router->post('/exercise/fulfillments', 'App\Controllers\exerciseController@fulfillments');
 $router->post('/exercise/update', 'App\Controllers\exerciseController@update');
+$router->post('/exercise/delete/', 'App\Controllers\exerciseController@delete');
 
 //Questions
 $router->get('/question/fields/:id', 'App\Controllers\questionController@fields');
--- public/js/main.js
+++ public/js/main.js
@@ -2,16 +2,17 @@ $(document).ready(function () {
   /**
    * delete element
    */
-  $(".fa-trash").click(function () {
-    let res = confirm("Are you sure?");
-    if (res === true) {
+  document.querySelectorAll('a[data-method="delete"]').forEach(item => {
+    item.addEventListener("click", function () { 
+      if (!confirm(item.dataset.confirm)) return false;
+
       $.post($(this).data("href"), { id: $(this).data("val") }, function () {
-        location.reload();
+        //location.reload();
       }).fail(function (result) {
+
         alert("Got error");
       });
-    }
-    return false;
+    })
   });
   
   /**
@@ -21,8 +22,7 @@ $(document).ready(function () {
     item.addEventListener("click", function () { 
       if (!confirm(item.dataset.confirm)) return false;
 
-      $.post($(this).data("href"), { id: $(
-        this).data("val"),status: $(this).data("status") }, function () {
+      $.post($(this).data("href"), { id: $(this).data("val"),status: $(this).data("status") }, function () {
        (window.location.href.includes("question")) && (item.dataset.method ="put") ? window.location.href = '/' : location.reload();
       }).fail(function (result) {
 
