d722b856c833fc84b0d58a6521856daeaec60cb2
melohan <melodie.ohan@cpnv.ch>
2021-12-13 09:02:00 +0100

    Make fulfillments page dynamic

--- resources/views/exercise/fulfillments.php
+++ resources/views/exercise/fulfillments.php
@@ -1,27 +1,33 @@
 <?php
-$headerText = '<span class="exercise-label">voila</span>';
+$exercise = $params['exercise'];
+$headerText = ($exercise != null) ? "<span class='exercise-label'>" . $exercise->getTitle() . "</span>" : "";
+$questions = isset($params['exercise']) ? $exercise->getQuestions() : null;
 $headerClass = "heading answering";
 ?>
+<?php if (is_array($questions) && count($questions) > 0): ?>
     <h1>Your take</h1>
     <p>If you'd like to come back later to finish, simply submit it with blanks</p>
 
-    <form action="/exercise/fulfillments" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden"
-                                                                                           value="&#x2713;"/><input
-                type="hidden" name="authenticity_token"
-                value="SKgOEkmC0lE3Qz3q+RHf0YOhlIhkJ/DX6m8TcOEG/Wn4UGF3gD0upgrsWUeXa0PY+8+o3cueaY1PjHu2xB/srQ=="/>
+    <form action="/answer/fulfillments/<?= $exercise->getId(); ?>" accept-charset="UTF-8" method="post">
 
-
-        <input type="hidden" value="490" name="fulfillment[answers_attributes][][field_id]"
-               id="fulfillment_answers_attributes__field_id"/>
         <div class="field">
-            <label for="fulfillment_answers_attributes__value">la réponse d</label>
-            <input type="text" name="fulfillment[answers_attributes][][value]"
-                   id="fulfillment_answers_attributes__value"/>
-
+            <?php foreach ($exercise->getQuestions() as $question): ?>
+                <label for="fulfillment_answers_attributes__value"><?= $question->getText(); ?></label>
+                <input type="hidden" value="<?= $question->getId(); ?>"
+                       name="fulfillment[answers][][questionId]"
+                       id="fulfillment_answers_attributes__field_id" />
+                <?php if ($question->getType()->getId() == \App\models\QuestionType::SINGLE): ?>
+                    <input type="text" name="fulfillment[answers][][value]"
+                           id="fulfillment_answers_attributes__value"/>
+                <?php else: ?>
+                    <textarea name="fulfillment[answers][][value]"
+                              id="fulfillment_answers_attributes__value"></textarea>
+                <?php endif; ?>
+            <?php endforeach; ?>
         </div>
 
         <div class="actions">
-            <a class="button" href="#">Save</a>
-            <!-- <input type="submit" name="commit" value="Save" data-disable-with="Save" /> -->
+            <button type="submit" value="Save">Save</button>
         </div>
     </form>
+<?php endif; ?>
