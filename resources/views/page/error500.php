c3aa5493b454f74570fb33023ded43de0ead4d31
melohan <melodie.ohan@cpnv.ch>
2021-12-20 13:43:56 +0100

    Add error() method and views

---
+++ resources/views/page/error500.php
@@ -0,0 +1,10 @@
+<?php
+$headerText = "<h1>Exercise<br>Looper</h1>";
+$headerClass = "dashboard";
+?>
+<h1>Error 500</h1>
+<p>Internal Server Error :(</p>
+<a class="button answering column" href="/">Go home</a>
+<?php
+
+?>
