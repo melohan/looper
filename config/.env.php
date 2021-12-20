12973aeaa6795f6e047fac22b78aba8d93d444cd
Sou <souphakone.samoutphonh@cpnv.ch>
2021-12-20 14:59:09 +0100

    Replace path string

--- config/.env.php
+++ config/.env.php
@@ -1,8 +1,10 @@
 <?php
+
 /**
  * This file contains project path constants
  **/
 
 define('PROJECT_ROOT', dirname($_SERVER["DOCUMENT_ROOT"]));
-define('VIEWS', sprintf("%s/resources/views/", PROJECT_ROOT));
+$site_url = str_replace('\\', '/', str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', PROJECT_ROOT));
+define('VIEWS', $site_url . DIRECTORY_SEPARATOR . 'resources/views' . DIRECTORY_SEPARATOR);
 define('SCRIPTS', dirname($_SERVER['SCRIPT_NAME']) . DIRECTORY_SEPARATOR);
