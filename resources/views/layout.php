8280fa4d8a8869a05f139ff3976e954c71052019
melohan <melodie.ohan@cpnv.ch>
2021-12-21 13:45:38 +0100

    Add documentation for header's variables

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -1,13 +1,22 @@
 <?php
+/**
+ * This document manages the display of headers for pages. Each layout can decide to customize the banner
+ * and in this case it will have to inform if:
+ * the css class, the text, if there is a link, the url, the text link etc..
+ */
+// default for home page
 if (!isset($cssClass)) {
     $cssClass = "dashboard";
     $text = "<h1>Exercise <br> Looper</h1>";
+// personalized case
 } else if (isset($text)) {
     $text = htmlentities($text);
+// no text case
 } else {
     $text = "";
 }
 
+//If title use URL
 if (!isset($textLink)) {
     $urlLink = "";
     $useLink = false;
@@ -28,29 +37,30 @@ if (!isset($textLink)) {
     <!-- Looper css -->
     <link rel="stylesheet" href="/css/looper.css">
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="/js/main.js"></script>
 </head>
 
 <body>
 
-    <header class="<?= $cssClass ?>">
-        <section class="container">
-            <a href="/"><img src="/img/logo.png" /></a>
-            <span class='exercise-label'>
+<header class="<?= $cssClass ?>">
+    <section class="container">
+        <a href="/"><img src="/img/logo.png"/></a>
+        <span class='exercise-label'>
                 <?= $text; ?>
-                <?php if ($useLink) : ?>
-                    <a href="<?= $urlLink; ?>"><?= $textLink; ?></a>
-                <?php endif; ?>
+            <?php if ($useLink) : ?>
+                <a href="<?= $urlLink; ?>"><?= $textLink; ?></a>
+            <?php endif; ?>
             </span>
-        </section>
-    </header>
+    </section>
+</header>
 
-    </br>
-    </br>
-    <div class="container dashboard">
-        <?= $content ?>
-    </div>
+</br>
+</br>
+<div class="container dashboard">
+    <?= $content ?>
+</div>
 
 </body>
 
