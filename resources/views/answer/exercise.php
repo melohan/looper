bfcc6385fe1dde7131c6c5c3d220dc8953e5d9ff
melohan <melodie.ohan@cpnv.ch>
2021-12-21 18:35:32 +0100

    Add check for foreach operation

--- resources/views/answer/exercise.php
+++ resources/views/answer/exercise.php
@@ -24,29 +24,31 @@ $users = $params['users'];
         </tr>
         </thead>
         <tbody>
-        <?php foreach ($users as $user): ?>
-            <tr>
-                <td>
-                    <a href="/answer/user/<?= $user->getId(); ?>/exercise/<?= $exercise->getId(); ?>"><?= $user->getName(); ?></a>
-                </td>
-
-                <?php
-                $answers = \App\Models\Answer::getAnswersByExercise($exercise->getId(), ['user_id' => $user->getId()]);
-                ?>
-
-                <?php foreach ($answers as $answer): ?>
-                    <td class="answer">
-                        <?php if (empty($answer->getAnswer())): ?>
-                            <i class="fa fa-times empty"></i>
-                        <?php elseif (strlen($answer->getAnswer()) > DOUBLE_FILLED): ?>
-                            <i class="fa fa-check-double filled"></i>
-                        <?php else: ?>
-                            <i class="fa fa-check short"></i>
-                        <?php endif; ?>
+        <?php if (!is_null($users)): ?>
+            <?php foreach ($users as $user): ?>
+                <tr>
+                    <td>
+                        <a href="/answer/user/<?= $user->getId(); ?>/exercise/<?= $exercise->getId(); ?>"><?= $user->getName(); ?></a>
                     </td>
-                <?php endforeach; ?>
-            </tr>
-        <?php endforeach; ?>
+
+                    <?php
+                    $answers = \App\Models\Answer::getAnswersByExercise($exercise->getId(), ['user_id' => $user->getId()]);
+                    ?>
+
+                    <?php foreach ($answers as $answer): ?>
+                        <td class="answer">
+                            <?php if (empty($answer->getAnswer())): ?>
+                                <i class="fa fa-times empty"></i>
+                            <?php elseif (strlen($answer->getAnswer()) > DOUBLE_FILLED): ?>
+                                <i class="fa fa-check-double filled"></i>
+                            <?php else: ?>
+                                <i class="fa fa-check short"></i>
+                            <?php endif; ?>
+                        </td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
         </tbody>
     </table>
 <?php endif; ?>
